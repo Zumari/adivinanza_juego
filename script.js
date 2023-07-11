5e0fb373ddbe7219445312c1a450a02c3d9c68a4
Zumari <fernanda_zunigaa@yahoo.com>
2023-07-11 01:38:01 -0600

    modificación

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ console.log(numeroAzar)
 
 let numeroEntrada = document.getElementById('numeroEntrada')
 let mensaje = document.getElementById("mensaje")
+let intento = document.getElementById("intento")
+let intentos = 0
 
 function chequearResultado() {
+
+    intentos++
+    intento.textContent = intentos
 
     let numeroIngresado = parseInt(numeroEntrada.value)
 
@@ -16,12 +21,15 @@ function chequearResultado() {
     if(numeroIngresado === numeroAzar){
         mensaje.textContent = 'Felicidades!! Has adivinado el número secreto';
         mensaje.style.color = 'green';
+        intento.style.color = 'green';
         numeroEntrada.disabled = true;
     }else if(numeroIngresado > numeroAzar){
         mensaje.textContent = 'El número ingresado es mayor que el número secreto';
         mensaje.style.color = 'red';
+        intento.style.color = 'red';
     }else{
         mensaje.textContent = 'El número ingresado es menor que el número secreto';
         mensaje.style.color = 'red';
+        intento.style.color = 'red';
     }
 }
