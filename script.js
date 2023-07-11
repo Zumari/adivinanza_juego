9055dabb53e48ba14d24d3679af2c0d7dd306e33
Zumari <fernanda_zunigaa@yahoo.com>
2023-07-11 01:17:27 -0600

    mejorado

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ function chequearResultado() {
 
     if(numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)){
         mensaje.textContent = 'Por favor, introduce un número válido entre 1 y 100'
-    }else if(numeroIngresado == numeroAzar){
-        mensaje.textContent = 'Felicidades!! Has adivinado el número secreto'
+        return;
+    }
+    
+    if(numeroIngresado === numeroAzar){
+        mensaje.textContent = 'Felicidades!! Has adivinado el número secreto';
+        mensaje.style.color = 'green';
+        numeroEntrada.disabled = true;
     }else if(numeroIngresado > numeroAzar){
-        mensaje.textContent = 'El número ingresado es mayor que el número secreto'
+        mensaje.textContent = 'El número ingresado es mayor que el número secreto';
+        mensaje.style.color = 'red';
     }else{
-        mensaje.textContent = 'El número ingresado es menor que el número secreto'
+        mensaje.textContent = 'El número ingresado es menor que el número secreto';
+        mensaje.style.color = 'red';
     }
 }
